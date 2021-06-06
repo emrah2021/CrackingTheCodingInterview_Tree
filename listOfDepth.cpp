1d3fef17d051283d988f2f8137a3b8c85e09e4f6
emrah2021 <82098888+emrah2021@users.noreply.github.com>
2021-06-06 18:04:35 +0300

    listOfDepth
    Given a binary tree, design an algorithm which creates a linked list of all the nodes
    at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
    O(n)

---
+++ listOfDepth.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <bitset>
+#include "Node.h"
+#include "Stack.h"
+#include "Tree.h"
+
+using namespace std;
+
+int main()
+{
+	int sortedArr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	TreeNode* sortedTree = new TreeNode();
+	sortedTree = sortedTree->createMinimalBST(sortedArr, 10);
+	sortedTree->printInOrder(sortedTree);
+	sortedTree->printPreOrder(sortedTree);
+	list<list<TreeNode*>> levelList = sortedTree->findLevelLinkedList(sortedTree);
+  cout << "list size: " << levelList.size();
+	cout << "list size: " << levelList.back().size() << endl;
+  
+  cin.get();
+	return 0;
+}
